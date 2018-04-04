074a47baed7c57fa0d25b73112fcf71cdcc785f1
codysk <me@iskywind.com>
2018-04-04 13:27:31 +0800

    first publish

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM alpine:3.7
+MAINTAINER me@iskywind.com
+
+VOLUME ["/bgmi"]
+
+ENV LANG=C.UTF-8 BGMI_PATH="/bgmi/conf/bgmi"
+ADD ./ /home/bgmi-docker
+
+RUN { \
+	apk add --update linux-headers gcc python-dev libffi-dev openssl-dev libxslt-dev zlib-dev libxml2-dev musl-dev nginx bash supervisor transmission-daemon python curl; \
+	curl https://bootstrap.pypa.io/get-pip.py | python; \
+	pip install 'requests[security]'; \
+	pip install 'transmissionrpc'; \
+	pip install /home/bgmi-docker/BGmi; \
+}
+
+EXPOSE 80 9091
+
+ENTRYPOINT ["/home/bgmi-docker/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+first_lock="/bgmi_install.lock"
+
+data_source="bangumi_moe"	#default data source set to bangumi.moe
+admin_token="bgmi_token" #default admin token
+
+function init_proc {
+	touch $first_lock
+
+	if [ ! -z $BGMI_ADMIN_TOKEN ]; then
+		admin_token=$BGMI_ADMIN_TOKEN
+	fi
+
+	if [ ! -z $BGMI_SOURCE ]; then
+		data_source=$BGMI_SOURCE
+	fi
+	bgmi install
+	bgmi source $data_source
+	bgmi config ADMIN_TOKEN $admin_token
+	bgmi config SAVE_PATH /bgmi/bangumi
+	bgmi config DOWNLOAD_DELEGATE transmission-rpc
+
+	mkdir -p /var/run/nginx
+	mkdir -p /bgmi/conf/bgmi
+	mkdir -p /bgmi/conf/transmission
+	mkdir -p /bgmi/log
+	mkdir -p /bgmi/bangumi
+	mkdir -p /etc/supervisor.d
+
+	cp /home/bgmi-docker/config/bgmi_nginx.conf /etc/nginx/conf.d/default.conf
+	cp /home/bgmi-docker/config/bgmi_supervisord.ini /etc/supervisor.d/bgmi_supervisord.ini
+	cp /home/bgmi-docker/config/transmission-daemon /etc/conf.d/transmission-daemon
+	cp /home/bgmi-docker/config/transmission_settings.json /bgmi/conf/transmission/settings.json
+}
+
+if [ ! -f $first_lock ]; then
+	init_proc
+fi
+
+/usr/bin/supervisord -n
